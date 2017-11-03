af1527d69e22bd790d458cd08444bddcaca6fa1b
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-11-04 00:54:29 +0100

    Add shell hook to default.nix to start users preferred shell

--- default.nix
+++ default.nix
@@ -2,5 +2,9 @@ with import <nixpkgs> {}; {
   nodeEnv = stdenv.mkDerivation {
     name = "graphql-playground";
     buildInputs = [ inotify-tools httpie ];
+    shellHook = ''
+    # Start user's preffered shell
+    exec ${builtins.getEnv "SHELL"}
+    '';
   };
 }
